071c764756a520637b4e7359519771fefb91560d
RichardMogg <157908469+RichardMogg@users.noreply.github.com>
2026-05-30 01:34:14 +0200

    asd

--- js/forms-availability.js
+++ js/forms-availability.js
@@ -110,19 +110,3 @@ async function isFormAvailable(form) {
     return false;
   }
 }
-[
-
-  {
-    "url": "forms/GBT_Störungsbericht/index.html",
-    "id": "GBT Störungsbericht",
-    "title": "GBT Störungsbericht",
-    "category": "Berichte",
-    "description": "",
-    "version": "1.0",
-    "status": "active",
-    "tags": ["GBT Störungsbericht", "Berichte"],
-    "favorite": false,
-    "acceptsOrderContext": true,
-    "prefillProfile": "standard"
-  }
-]
